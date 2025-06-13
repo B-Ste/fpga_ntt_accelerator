74f236c6ade2a19a83b7ca501f6b711b98f40ab8
Benjamin Steeg <benni.steeg@icloud.com>
2025-06-13 16:33:51 +0200

    use inferred multiplier

--- src/modular_arithmetic/modular_multiplier.v
+++ src/modular_arithmetic/modular_multiplier.v
@@ -8,18 +8,9 @@ module modular_multiplier (
 
     parameter mod_index = 0;
 
-    wire [59:0]mult_out;
-
-    dsp_multiplier mult (
-        .CLK(clk),  // input wire CLK
-        .A(a),      // input wire [29 : 0] A
-        .B(b),      // input wire [29 : 0] B
-        .P(mult_out)      // output wire [59 : 0] P
-    );
-
     windowed_reduction60bit #mod_index reduction(
 	    .clk(clk), 
-        .in(mult_out), 
+        .in(a * b), 
         .out(c));
 
 endmodule
